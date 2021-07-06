00c019fe9ef511bb372594d119c8cb796e12120b
alearm <alearm246@gmail.com>
2021-07-06 09:54:53 -0500

    added authenticated user route

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -6,6 +6,18 @@ const getUsers = async (req, res) => {
     }
 }
 
+const currentUser = async (req, res) => {
+    try {
+        const currentUser = req.user;
+        if(!currentUser) return res.status(404).send("couldn't retrieve user");
+
+        return res.status(200).send(currentUser);
+    } catch(err){
+        console.error(err);
+    }
+}
+
 module.exports = {
     getUsers,
+    currentUser
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { getUsers } = require("../controllers/user");
+const { getUsers,currentUser } = require("../controllers/user");
 
 router.get("/", getUsers);
+router.get("/current", currentUser);
 
 module.exports = router;
