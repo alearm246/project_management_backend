4249d8fc49efebbb868a3ee5569211b20b1f7862
alearm <alearm246@gmail.com>
2021-07-07 15:34:54 -0500

    created junction table for users and clans

--- src/models/Clan.js
+++ src/models/Clan.js
@@ -8,7 +8,6 @@ class Clan {
 
             return users;
         } catch(err) {
-            console.error(err);
             throw new Error(err);
         }
     }
@@ -21,27 +20,32 @@ class Clan {
             if(duplicateClan) throw new Error("clan with that name already exists");
 
             const result = await db.query(
-                `INSERT INTO clans (name, game, user_id) VALUES ($1, $2, $3) RETURNING *`,
-                [name, game, userId]
+                `INSERT INTO clans (name, game) VALUES ($1, $2) RETURNING *`,
+                [name, game]
             )
 
             const createdClan = result.rows[0];
+
+            const userAndClanResult = await db.query(
+                `INSERT INTO users_clans (user_id, clan_id) VALUES ($1, $2) RETURNING *`,
+                [userId, createdClan.id]
+            );
             
             return createdClan;
         } catch(err) {
-            console.error(err);
             throw new Error(err);
         }
     }
 
-    static async findByUserId(id) {
+    static async findByUserId(userId) {
         try {
-            const result = await db.query(`SELECT * FROM clans WHERE user_id = $1`, [id]);
+            const result = await db.query(
+                `SELECT clans.id, clans.name, clans.game, users_clans.user_id FROM clans INNER JOIN users_clans ON clans.id = users_clans.clan_id WHERE users_clans.user_id = $1`, 
+            [userId]);
             const clans = result.rows;
-
+            console.log(clans);
             return clans;
         } catch(err) {
-            console.error(err);
             throw new Error(err);
         }
     }
--- src/schemas/clans.sql
+++ src/schemas/clans.sql
@@ -1,6 +1,5 @@
 CREATE TABLE clans (
     id SERIAL PRIMARY KEY,
     name varchar(255) UNIQUE NOT NULL,
-    game varchar(255) NOT NULL,
-    user_id INT REFERENCES users(id)
+    game varchar(255) NOT NULL
 );
